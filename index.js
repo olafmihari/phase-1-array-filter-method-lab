63f925a39d11030e701dce83c96a2b128fd5c1ac
Olaf Mihari <olafnjenga@gmail.com>
2023-03-21 20:44:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+
+function findMatching(drivers, query) {
+    return drivers.filter((driver) => driver.toLowerCase() === query.toLowerCase());
+  }
+
+
+  function matchName(drivers, query) {
+    return drivers.filter((driver) => driver.name === query);
+}
+
+function fuzzyMatch(drivers, query) {
+    return drivers.filter((driver) => driver.toLowerCase().startsWith(query.toLowerCase()))
+}
